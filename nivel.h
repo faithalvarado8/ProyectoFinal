fb69907302cc9ff7f4e4cecbcf5dd07188aca46b
CarlaZapata14 <carla.zapata@udea.edu.co>
2024-11-04 01:32:52 -0500

    Create nivel.h
    Definicion de algunos atributos y metodos

---
+++ nivel.h
@@ -0,0 +1,17 @@
+#ifndef NIVEL_H
+#define NIVEL_H
+
+#include "jugador.h"
+
+class Nivel
+{
+private:
+    unsigned int numNivel;
+
+public:
+    Nivel();
+    Jugador * jugador;
+
+};
+
+#endif // NIVEL_H
