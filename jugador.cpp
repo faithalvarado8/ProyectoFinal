09cd4a8e31fc7983f94756d586cd96b17ef6f40b
CarlaZapata14 <carla.zapata@udea.edu.co>
2024-11-04 01:32:04 -0500

    Create jugador.cpp

---
+++ jugador.cpp
@@ -0,0 +1,8 @@
+#include "jugador.h"
+#include <QKeyEvent>
+
+Jugador::Jugador(QGraphicsItem * parent, string nombre){}
+
+void Jugador::keyPressEvent(QKeyEvent * event){}
+
+void Jugador::actualizarAnimacion(){}
