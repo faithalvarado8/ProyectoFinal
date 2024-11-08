33fd8f50c093c1ac91fa7a865797b9b1ee203ce7
CarlaZapata14 <carla.zapata@udea.edu.co>
2024-11-08 16:35:32 -0500

    Update juego.h
    Creación botones para iniciar cada nivel

--- juego.h
+++ juego.h
@@ -5,14 +5,28 @@
 #include <QWidget>
 #include <QGraphicsScene>
 #include <QGraphicsView>
+#include <QPushButton>
 
 class Juego: public QGraphicsView{
-
+    Q_OBJECT
 public:
     QGraphicsScene * escena;
     Nivel * nivel;
     Juego(QWidget * parent=nullptr);
     ~Juego();
+
+    void mostrarMenuInicio();
+    void iniciarNivel(int nivelSeleccionado);
+
+private:
+    QPushButton *botonNivel1;
+    QPushButton *botonNivel2;
+    QPushButton *botonNivel3;
+
+private slots:
+    void seleccionarNivel1();
+    void seleccionarNivel2();
+    void seleccionarNivel3();
 };
 
 #endif // JUEGO_H
