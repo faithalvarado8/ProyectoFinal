5aa5087d507039c2f8cc81084d4a8b8c93da5aa7
CarlaZapata14 <carla.zapata@udea.edu.co>
2024-11-10 01:35:39 -0500

    Update nivel.cpp

--- nivel.cpp
+++ nivel.cpp
@@ -1,7 +1,15 @@
 #include "nivel.h"
 
-Nivel::Nivel(int nivelSeleccionado): nivelSeleccionado(nivelSeleccionado) {
-    jugador = new Jugador();
+Nivel::Nivel(int nivelSeleccionado, QGraphicsScene * escena): nivelSeleccionado(nivelSeleccionado) {
+    if (nivelSeleccionado==3){
+
+        jugador = new Jugador("Bart");
+        //Enfocar personaje
+        jugador->setFlag(QGraphicsItem::ItemIsFocusable);
+        jugador->setFocus();
+
+        escena->addItem(jugador);
+    }
 }
 
 Nivel::~Nivel() {
