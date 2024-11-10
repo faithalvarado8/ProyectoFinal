b491cddfdf2674f4211c6feabaa1742da6dea570
CarlaZapata14 <carla.zapata@udea.edu.co>
2024-11-10 15:41:51 -0500

    Update jugador.h

--- jugador.h
+++ jugador.h
@@ -13,20 +13,29 @@ class Jugador: public Personajes{
     Q_OBJECT
 private:
     string nombre;
-    QPixmap sprites;
-    int ancho;
-    int alto;
+
+    // Hojas de sprites
+    QPixmap sprites;            // Hoja de sprites actualmente activa
+    QPixmap spritesLado;        // Hoja de sprites para movimiento lateral
+    QPixmap spritesArribaAbajo; // Hoja de sprites para movimiento vertical
+
+    // Dimensiones de los cuadros de cada hoja de sprites
+    int anchoLado, altoLado;         // Dimensiones para la hoja de movimiento lateral
+    int anchoArribaAbajo, altoArribaAbajo; // Dimensiones para la hoja de movimiento vertical
+    int ancho, alto;                 // Dimensiones actuales de los cuadros en uso
+
     int columna;
     int fila;
+
     QTimer *timer;
 
     QMap<int,bool>keys;
 
 public:
-    explicit Jugador(string nombre);
+    Jugador(string nombre);
 
-    void keyPressEvent(QKeyEvent * event)override;
-    void keyReleaseEvent(QKeyEvent *event)override;
+    void keyPressEvent(QKeyEvent * event);
+    void keyReleaseEvent(QKeyEvent *event);
 
 private slots:
     void actualizarAnimacion();
