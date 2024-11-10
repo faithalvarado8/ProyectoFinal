84e38cfb48893f9810c209fee70cfae688ae205d
CarlaZapata14 <carla.zapata@udea.edu.co>
2024-11-10 15:42:14 -0500

    Update jugador.cpp

--- jugador.cpp
+++ jugador.cpp
@@ -6,10 +6,22 @@
 
 Jugador::Jugador(string nombre){
     if (nombre=="Bart"){
-        sprites= QPixmap(":/Nivel3/BartLado.png");
+        spritesLado= QPixmap(":/Nivel3/BartLado.png");
+        spritesArribaAbajo = QPixmap(":/Nivel3/BartFrente.png");
 
         ancho=117;
         alto=187;
+
+        // Dimensiones de cada hoja de sprites
+        anchoLado = 117;
+        altoLado = 187;
+        anchoArribaAbajo = 45;
+        altoArribaAbajo = 88;
+
+
+        sprites = spritesLado; // Iniciar con la hoja de movimientos laterales
+        ancho = anchoLado;
+        alto = altoLado;
         columna = 0;
         fila = 0;
 
@@ -23,58 +35,5 @@ Jugador::Jugador(string nombre){
 
         setPos(35,65);
         setZValue(1);
-    }
-}
-
-void Jugador::actualizarAnimacion(){
-    bool moving = false;
-
-    if (keys[Qt::Key_Left] && pos().x()>35){
-        setPos(x()-10,y());
-        fila=1;
-        moving = true;
-    }
-    else if (keys[Qt::Key_Right] && pos().x()<1150){
-        setPos(x()+10,y());
-        fila=0;
-        moving = true;
-    }
-
-    if (moving){
-        // Avanzar al siguiente cuadro en la hoja de sprites
-        columna = (columna + 1) % 4;
-    }else {
-        columna = 0;
-    }
-  
-    // Calcular la posicion del cuadro en la hoja de sprites
-    int x = columna * ancho;
-    int y = fila * alto;
-
-    // Actualizar la imagen del personaje
-    QPixmap sprite = sprites.copy(x, y, ancho, alto);
-    setPixmap(sprite.scaled(80, 80, Qt::KeepAspectRatio, Qt::SmoothTransformation));
-
-}
-
-void Jugador::keyReleaseEvent(QKeyEvent *event) {
-    switch(event->key()){
-    case Qt::Key_Left:
-    case Qt::Key_Right:
-        keys[event->key()]=false;
-        break;
-    default:
-        break;
-    }
-}
-void Jugador::keyPressEvent(QKeyEvent *event)
-{
-    switch(event->key()){
-    case Qt::Key_Left:
-    case Qt::Key_Right:
-        keys[event->key()]=true;
-        break;
-    default:
-        break;
     }
 }
